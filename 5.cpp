de58c5cbe5387cf409a4413419446424c58b49ba
HamzaAdeel05 <hamza.adeel.2005@gmail.com>
2025-01-30 00:25:18 +0500

    Add files via upload

---
+++ 5.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main (){
+int num1=0,num2=0,product=0;
+cout<<"Enter number 1:";
+cin>>num1;
+cout<<"Enter number 2:";
+cin>>num2;
+product=num1*num2;
+cout<<"The Product is:"<<product;
+
+
+return 0;
+}
