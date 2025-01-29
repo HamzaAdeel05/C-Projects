43644d6a06f28b34c1e0ff9c2833b8d536fe9db3
HamzaAdeel05 <hamza.adeel.2005@gmail.com>
2025-01-30 00:25:01 +0500

    Add files via upload

---
+++ 4.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main (){
+int num1=0,num2=0,sum=0;
+cout<<"Enter number 1:";
+cin>>num1;
+cout<<"Enter number 2:";
+cin>>num2;
+sum=num1+num2;
+cout<<"The sum is:"<<sum;
+
+
+return 0;
+}
