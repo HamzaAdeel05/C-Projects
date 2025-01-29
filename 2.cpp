b83f0897ac80a7cac505dfb65000e05e3a34b44c
HamzaAdeel05 <hamza.adeel.2005@gmail.com>
2025-01-30 00:24:21 +0500

    Add files via upload

---
+++ 2.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+using namespace std;
+int main (){
+cout<<"To ensure academic excellence through deliverance of quality education and applied research in a collegiate enviroment having strong linkage with industry and international community to meet the societa challenges";
+return 0;
+}
